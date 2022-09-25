ccd8aad04bc055cfca8ec0627f8e1fd46ef3c328
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2022-09-25 21:50:23 +0300

    hgf

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const Server = require("socket.io");
-
-const io = new Server({});
-
-io.on("connection", (socket) => {
-    socket.on("disconnect", (reason) => {
-        console.log(reason);
-      });
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+io.on('connection', socket => {
+    while (1==1) {
+        console.log("fjds")
+        io.emit("hello", "world");
+    }
 });
-
-io.listen(3000);
+server.listen(3000);
